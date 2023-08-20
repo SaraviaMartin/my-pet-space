fa1907cadd08a7df0d9a458689eb8ac74b92db12
Juan <saraviamartin16@hotmail.com>
2023-08-20 17:00:42 -0300

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { Header } from './components/Header'
 import './App.css'
 import { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
+
 
 function App() {
   const [state, setState] = useState('Click me');
@@ -18,7 +27,22 @@ function App() {
     <div>
       <h1>Esta es la app</h1>
       <Header />
-      <button onClick={handleChange}>{state}</button>
+      <Button onClick={handleChange} variant="contained">{state}</Button>
+      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+      <Fab disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+    </Box>
     </div>
   );
 }
